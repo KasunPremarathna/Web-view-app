f209ce8fde7633965dec6a1758cb8e18cb83b62e
KasunPremarathna <htckasun@gmail.com>
2025-04-24 09:14:33 +0530

    NDK vertion roll back

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "info.mybikes.mybikeapp"
-    compileSdk = 35
+    compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "info.mybikes.mybikeapp"
+        applicationId = "com.example.web_view_application"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
